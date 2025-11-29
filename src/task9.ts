c0d402dcc046b4fb9fd2ff15042f5d8fd4670672
pattama422 <pattama@camt.info>
2025-11-29 16:57:29 +0700

    Add variable tpe

--- src/task9.ts
+++ src/task9.ts
@@ -1,4 +1,9 @@
-let i = 0;
+let i: number = 0;
 for (i = 0; i < 10; i++) {
   console.log(i);
+}
+
+let color: string[] = ["red", "blue", "green", "yellow", "orange", "purple"];
+for (let i = 0; i < color.length; i++) {
+  console.log(color[i]);
 }
