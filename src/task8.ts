c23a5f980ffc4fec73add83eb6a6ec5eaec71184
pattama422 <pattama@camt.info>
2025-11-29 16:44:05 +0700

    Add task8-3

--- src/task8.ts
+++ src/task8.ts
@@ -12,3 +12,12 @@ if (x2 > 5 && y2 > 12) {
 } else {
   console.log(x2 * y2 + x2 / y2);
 }
+
+let x3 = 4;
+if (x3 > 10) {
+  console.log("x3 is more than 10");
+} else if (x3 < 5) {
+  console.log("x3 is less than 5");
+} else {
+  console.log("x3 is between 5 and 10");
+}
