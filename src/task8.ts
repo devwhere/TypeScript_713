1726af801cb1abac9f52d6f2131ca23c4c61e7c7
pattama422 <pattama@camt.info>
2025-11-29 16:40:13 +0700

    add task8-2

--- src/task8.ts
+++ src/task8.ts
@@ -4,3 +4,11 @@ if (x > 10) {
 } else {
   console.log("x is less than or equal to 10");
 }
+
+let x2: number = 10;
+let y2: number = 13;
+if (x2 > 5 && y2 > 12) {
+  console.log("คำมากจัง");
+} else {
+  console.log(x2 * y2 + x2 / y2);
+}
