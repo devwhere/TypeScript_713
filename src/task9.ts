79c294d963b87fe76a6649728d964bf21227a220
pattama422 <pattama@camt.info>
2025-12-03 14:06:24 +0700

    task9-3

--- src/task9.ts
+++ src/task9.ts
@@ -7,3 +7,12 @@ let color: string[] = ["red", "blue", "green", "yellow", "orange", "purple"];
 for (let i = 0; i < color.length; i++) {
   console.log(color[i]);
 }
+
+let names: string[] = ["Tui", "Pattama", "Easter", "Superman", "December"];
+for (const name of names) {
+  if (name.length > 6) {
+    console.log("สวัสดีคุณ " + name);
+  } else {
+    console.log("Hello " + name);
+  }
+}
